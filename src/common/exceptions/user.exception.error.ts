a2281868c5ad790f597f3fea16ca714f43075343
cuijian <1453475013@qq.com>
2022-11-29 15:28:38 +0800

    chore: 添加错误拦截

--- src/common/exceptions/user.exception.error.ts
+++ src/common/exceptions/user.exception.error.ts
@@ -1,10 +1,17 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { ApiErrorCode } from '@src/enums/api-error-code.enum';
 
+/**
+ * 自定义错误处理
+ * @example
+ *  throw new UserException()
+ *  throw new UserException('错误处理', 9999)
+ *  throw new UserException('用户名/手机号码/邮箱已经存在，不能重复创建', 9999, { name: 222,test: '22290',})
+ */
 export class UserException extends HttpException {
-  public static message = '系统错误';
-  public static code = 9999;
-
-  constructor(msg?: string | Record<string, any>, code?: number, ...oth: any[]) {
+  private static message = '系统错误';
+  private static code = ApiErrorCode.ERROR;
+  constructor(msg?: string | Record<string, any>, code?: ApiErrorCode, ...oth: any[]) {
     super(
       { code: code ?? UserException.code, message: msg ?? UserException.message, ...oth },
       HttpStatus.OK
