bd1dabebc1d3ef43c7f65c2a812a5ff2f8e3f8bd
cuijian <1453475013@qq.com>
2022-12-01 22:12:35 +0800

    chore: 权限管理

---
+++ src/common/decorators/user.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const User = createParamDecorator((data: string, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  console.log('request', request);
+  const user = request.username;
+  console.log('data', data, user);
+  return data ? user?.[data] : user;
+});
